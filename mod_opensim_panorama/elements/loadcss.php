a11dd1c2fc5d760eec8a85bc001a52ecdba98696
djphil <djphil@users.noreply.github.com>
2018-08-15 16:42:22 +0200

    Indentation
    Indentation

--- mod_opensim_panorama/elements/loadcss.php
+++ mod_opensim_panorama/elements/loadcss.php
@@ -13,32 +13,32 @@ jimport('joomla.form.formfield');
 
 class JFormFieldloadcss extends JFormField
 {
-	protected $type = 'loadcss';
+    protected $type = 'loadcss';
 
-	protected function getInput() 
-	{
-	   // return '';
-	}
+    protected function getInput() 
+    {
+        // return '';
+    }
 
-	protected function getLabel()
-	{
-		// Add custom admin css here
-		$cssFile = $this->element['file'] ? (string) $this->element['file'] : "loadcss.css";
-		$cssDir  = $this->element['path'] ? (string) $this->element['path'] : substr(__dir__, strlen(JPATH_ROOT) + 1);
-		$cssPath = $cssDir . DIRECTORY_SEPARATOR . $cssFile;
-		JHtml::stylesheet($cssPath);
-		// return '';
-	}
+    protected function getLabel()
+    {
+        // Add custom admin css here
+        $cssFile = $this->element['file'] ? (string) $this->element['file'] : "loadcss.css";
+        $cssDir  = $this->element['path'] ? (string) $this->element['path'] : substr(__dir__, strlen(JPATH_ROOT) + 1);
+        $cssPath = $cssDir . DIRECTORY_SEPARATOR . $cssFile;
+        JHtml::stylesheet($cssPath);
+        // return '';
+    }
 
-	/**
+    /**
 	 * Method to get the field title.
 	 *
 	 * @return  string  The field title.
 	 *
 	 * @since   11.1
 	 */
-	protected function getTitle()
-	{
-		// return $this->getLabel();
-	}
+    protected function getTitle()
+    {
+        // return $this->getLabel();
+    }
 }
